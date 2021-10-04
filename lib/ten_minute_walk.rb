fc8d18538920bb621b7e78163432cd51b63af013
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-10-04 09:17:58 +0100

    more e than w

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,5 +1,7 @@
 def ten_minute_walk(directions_to_walk)
   if directions_to_walk.length < 10 || directions_to_walk.count('s') > directions_to_walk.count('n') || directions_to_walk.count('s') < directions_to_walk.count('n') || directions_to_walk.count('w') > directions_to_walk.count('e')
     false
+  elsif directions_to_walk.count('w') < directions_to_walk.count('e')
+    false
   end
 end
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -16,5 +16,11 @@ describe 'ten minute walk app' do
   it 'is walking more west than east' do
     expect(ten_minute_walk(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to equal false 
   end 
+
+  it 'is walking more east than west' do
+    expect(ten_minute_walk(['e', 's', 'w', 'n', 'n', 'w', 's', 'e', 'e', 'e' ])).to equal false 
+  end 
 end
+
+
 
